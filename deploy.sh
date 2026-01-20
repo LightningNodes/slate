4c17a8f6b2c51b941e18ad6ae982a44cc84e6282
Rahul <rahul@pi42.com>
2026-01-20 16:23:23 +0530

    Updated options websocket documentation

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,10 @@ Options:
 #   bundle exec middleman build --clean
 # }
 
+#Run
+#docker run --rm --name slate -p 4567:4567 -v $(pwd)/source:/srv/slate/source slatedocs/slate serve 
+
+#Build
 #docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate build
 
 parse_args() {
