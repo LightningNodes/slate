b2cc833b92acc07156e53723ec2cd16c88f0b8ba
Rahul <rahul@pi42.com>
2026-01-22 17:54:25 +0530

    New api and error codes

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,9 @@ Options:
 #   bundle exec middleman build --clean
 # }
 
+#Run command : docker run --rm --name slate -p 4567:4567 -v $(pwd)/source:/srv/slate/source slatedocs/slate serve
+#Build command: docker run --rm --name slate -v $(pwd)/build:/srv/slate/build -v $(pwd)/source:/srv/slate/source slatedocs/slate build
+
 parse_args() {
   # Set args from a local environment file.
   if [ -e ".env" ]; then
